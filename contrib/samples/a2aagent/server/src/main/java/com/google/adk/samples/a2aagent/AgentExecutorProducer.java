e3cbbc3296b0411519b00c37587f086c76a1340c
Anastasiia Dudarenko <adudarenko@google.com>
2026-01-28 13:43:23 +0000

    fix pom xml

--- contrib/samples/a2aagent/server/src/main/java/com/google/adk/samples/a2aagent/AgentExecutorProducer.java
+++ contrib/samples/a2aagent/server/src/main/java/com/google/adk/samples/a2aagent/AgentExecutorProducer.java
@@ -1,26 +1,28 @@
 package com.google.adk.samples.a2aagent;
 
-import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.enterprise.inject.Produces;
-
-import com.google.adk.samples.a2aagent.agent.Agent;
 import com.google.adk.runner.Runner;
+import com.google.adk.samples.a2aagent.agent.Agent;
 import com.google.adk.sessions.InMemorySessionService;
 import io.a2a.server.agentexecution.AgentExecutor;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.inject.Produces;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 @ApplicationScoped
 public class AgentExecutorProducer {
 
-    @ConfigProperty(name = "my.adk.app.name", defaultValue = "default-app")
-    String appName;
+  @ConfigProperty(name = "my.adk.app.name", defaultValue = "default-app")
+  String appName;
 
-    @Produces
-    public AgentExecutor agentExecutor() {
-        InMemorySessionService sessionService = new InMemorySessionService();
-        Runner runnerInstance =
-            new Runner.Builder().agent(Agent.ROOT_AGENT).appName(appName).sessionService(sessionService).build();
-        return new com.google.adk.a2a.AgentExecutor(runnerInstance);
-
-    }
+  @Produces
+  public AgentExecutor agentExecutor() {
+    InMemorySessionService sessionService = new InMemorySessionService();
+    Runner runnerInstance =
+        new Runner.Builder()
+            .agent(Agent.ROOT_AGENT)
+            .appName(appName)
+            .sessionService(sessionService)
+            .build();
+    return new com.google.adk.a2a.AgentExecutor(runnerInstance);
+  }
 }
